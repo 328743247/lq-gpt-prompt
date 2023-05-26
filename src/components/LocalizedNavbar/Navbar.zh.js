ce021df22511de6956efd71a71da6d4bc545c762
liuqiang <liuqiang01@mucang.cn>
2023-05-26 11:05:55 +0800

    4444

--- src/components/LocalizedNavbar/Navbar.zh.js
+++ src/components/LocalizedNavbar/Navbar.zh.js
@@ -11,33 +11,21 @@ const zhNavbar = {
       label: "ChatGPT 白嫖站",
       position: "left",
     },
-/*    {
+   {
       type: 'dropdown',
       label: '其他工具',
       position: 'left',
       items: [
         {
-          label: "Chrome 扩展版",
-          href: "https://chrome.google.com/webstore/detail/chatgpt-shortcut/blcgeoojgdpodnmnhfpohphdhfncblnj",
+          label: "ChatGpt白嫖站",
+          href: "http://aiasker.online",
         },
         {
-          label: 'IMGPrompt',
-          href: 'https://prompt.newzone.top/',
-        },
-        {
-          label: '文字处理',
-          href: 'https://tools.newzone.top/',
-        },
-        {
-          label: '工具收藏',
-          href: 'https://nav.newzone.top',
-        },
-        {
-          label: 'Find on Product Hunt',
-          href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
+          label: '购买open ai账号',
+          href: 'http://salekey.online/',
         },
       ],
-    },*/
+    },
     // {
     //   to: "https://xq7pd3qu17.feishu.cn/share/base/form/shrcnK7oaSPaAcj1YAHfy4jo0Jh",
     //   label: "反馈/添加提示词",
